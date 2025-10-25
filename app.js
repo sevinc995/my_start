c01d7fce3741f1129030d806b12bf3b60a1490a0
sevinc995 <sevincseferova25@gmail.com>
2025-10-25 17:23:07 +0000

    commited

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 "use strict";
 $(document).ready(function () {
-
+    let menuList = document.getElementById("menulist")
+    menuList.style.maxHeight = "0px";
+    function toggleMenu(){
+        if(menuList.style.maxHeight == "0px")
+        {
+            menuList.style.maxHeight = "300px";
+        }
+        else
+            menuList.style.maxHeight = "0px"
+    }
 });
