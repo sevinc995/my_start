a37a5f491094f84015fe5ab32d3e393864079aef
sevinc995 <sevincseferova25@gmail.com>
2025-10-27 10:10:18 +0000

    changed

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 "use strict";
-$(document).ready(function () {
-    let menuList = document.getElementById("menulist")
-    menuList.style.maxHeight = "0px";
-    function toggleMenu(){
-        if(menuList.style.maxHeight == "0px")
-        {
-            menuList.style.maxHeight = "300px";
-        }
-        else
-            menuList.style.maxHeight = "0px"
-    }
-});
+
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+function toggleMenu() {
+    if (menuList.style.maxHeight === "0px") {
+        menuList.style.maxHeight = "300px";
+    } else{
+        menuList.style.maxHeight = "0px";
+    }      
+}
+
