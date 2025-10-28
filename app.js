76612599a9925ca58c7475eb5ce29a1e34e0a5f1
sevinc995 <sevincseferova25@gmail.com>
2025-10-28 12:35:09 +0000

    changed

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 let menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 function toggleMenu() {
-    if (menuList.style.maxHeight === "0px") {
-        menuList.style.maxHeight = "300px";
+    if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+        menuList.style.maxHeight = "250px";
     } else{
         menuList.style.maxHeight = "0px";
     }      
