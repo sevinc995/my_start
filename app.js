1f5fb38bc00b47c6f6d1d2b2907a90a55389cc6c
sevinc995 <sevincseferova25@gmail.com>
2025-11-16 12:23:26 +0000

    commited

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ menuList.style.maxHeight = "0px";
 function toggleMenu() {
     if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
         menuList.style.maxHeight = "250px";
-    } else{
+    } else {
         menuList.style.maxHeight = "0px";
-    }      
+    }
 }
 
+    document.getElementById("input-box").addEventListener("submit", function (e) {
+        e.preventDefault();
+        let nameField = document.getElementById("fullname");
+        let nameError = nameField.nextElementSibling;
+
+        if (nameField.value.trim() === "") {
+            nameField.classList.add("error-border");
+            nameError.style.display = "block";
+        } else {
+            nameField.classList.remove("error-border");
+            nameError.style.display = "none";
+        }
+    })
