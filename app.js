808150c156739ef2b8470c5565a5bae7bb27ba28
sevinc995 <sevincseferova25@gmail.com>
2025-11-19 07:24:03 +0000

    commited

--- app.js
+++ app.js
@@ -10,16 +10,49 @@ function toggleMenu() {
     }
 }
 
-    document.getElementById("input-box").addEventListener("submit", function (e) {
-        e.preventDefault();
-        let nameField = document.getElementById("fullname");
-        let nameError = nameField.nextElementSibling;
+    // document.getElementById("contact-form").addEventListener("submit", function (e) {
+    //     e.preventDefault();
+    //     // fullname
+    //     let nameField = document.getElementById("fullname");
+    //     let nameError = nameField.nextElementSibling;
 
-        if (nameField.value.trim() === "") {
-            nameField.classList.add("error-border");
-            nameError.style.display = "block";
-        } else {
-            nameField.classList.remove("error-border");
-            nameError.style.display = "none";
-        }
-    })
+
+    //     if (nameField.value.trim() === "") {
+    //         nameField.classList.add("error-border");
+    //         nameError.style.display = "block";
+    //     } else {
+    //         nameField.classList.remove("error-border");
+    //         nameError.style.display = "none";
+    //     }
+
+    //     // email
+    //     let emailField = document.getElementById("email");
+    //     let emailError = emailField.nextElementSibling;
+
+    //     if (emailField.value.trim() === "") {
+    //         emailField.classList.add("error-border");
+    //         emailError.style.display = "block";
+    //     } else {
+    //         emailField.classList.remove("error-border");
+    //         emailError.style.display = "none";
+    //     }
+
+    //     // phone
+    //     let phoneField = document.getElementById("phone");
+    //     let phoneError = phoneField.nextElementSibling;
+
+
+    //     if (phoneField.value.trim() === "") {
+    //         phoneField.classList.add("error-border");
+    //         phoneError.style.display = "block";
+    //     } else {
+    //         phoneField.classList.remove("error-border");
+    //         phoneError.style.display = "none";
+    //     }
+    // })
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+
+});
