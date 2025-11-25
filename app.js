1ba69fd67bc5805becc1ecc8de738a48064c0f45
sevinc995 <sevincseferova25@gmail.com>
2025-11-25 10:34:22 +0000

    commit

--- app.js
+++ app.js
@@ -11,30 +11,49 @@ function toggleMenu() {
 }
 
 // images
-let modal = document.getElementById("ïmgModal");
-let modalImg = document.getElementById("modalImg");
-let closeBtn = document.querySelector(".close");
-let images = document.querySelectorAll(".portfolio-img");
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
 
-images.forEach(img => {
-    img.addEventListener("click", function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    });
-});
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
 
-closeBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-});
+var slideIndex = 1;
+showSlides(slideIndex);
 
-modal.addEventListener("click", function (e){
-    if (e.target === modal) {
-        modal.style.display = "none";
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-});
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
 // form
-$(document).ready(function() {
+$(document).ready(function () {
 
     const form = $('#contact-form')[0];
     const inputs = $('#contact-form input');
